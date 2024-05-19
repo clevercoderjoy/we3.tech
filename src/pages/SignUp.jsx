9e1bb39d53524adabcea19424ee9ced2f3f5b5b6
clevercoderjoy <clevercoderjoy@gmail.com>
2024-05-19 13:53:19 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -19,8 +19,9 @@ const SignUp = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className='flex flex-col items-center'>
       <input
+        className='mt-8 border-black border-2 rounded px-4 py-2 font-bold'
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
@@ -28,6 +29,7 @@ const SignUp = () => {
         autoComplete="email"
       />
       <input
+        className='mt-8 border-black border-2 rounded px-4 py-2 font-bold'
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
